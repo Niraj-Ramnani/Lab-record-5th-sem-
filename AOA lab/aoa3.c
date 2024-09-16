3b07b2bae1c9619649245333dfd0e9208c1470fb
Niraj-Ramnani <neerajramnani800@gmail.com>
2024-09-16 23:21:54 +0530

    program 3 was updated

--- AOA lab/aoa3.c
+++ AOA lab/aoa3.c
@@ -1,8 +1,8 @@
-// merge sort
+// aim : write a c program to implement quick sort 
+// description : It is a sorting algorithm based on the Divide and Conquer that picks an element as a pivot and partitions the given array around the picked pivot by placing the pivot in its correct position in the sorted array . Quick sort average time complexity is O (n log n)
 #include<stdio.h>
 //#include<conio.h>
-void merge_sort(int arr[] , int first , int last);
-void merge(int arr[] , int first , int mid , int last);
+void quick_sort(int arr[] , int first , int last);
 void display_arr(int arr[] , int n);
 void main(){
 int i,n;
@@ -13,50 +13,41 @@ scanf("%d",&n);
 printf("Enter the elements of the array \n");
 for(i =0; i<n; i++){
 scanf("%d",&arr[i]);}
-merge_sort(arr,0,n-1);
+quick_sort(arr,0,n-1);
 printf("Elements after sorting ");
 display_arr(arr,n);
 //getch();
 }
-void merge_sort(int arr[],int first ,int last){
-int mid;
-if(first>=last){
-return;}
-mid = (first + last )/2;
-merge_sort(arr,first,mid);
-merge_sort(arr,mid + 1,last);
-merge(arr,first,mid,last);
-}
-void merge(int arr[] , int first , int mid , int last ){
-int left,right,i,j;
-int temp[100];
-int ptr = 0;
-left = first;
-right = mid + 1;
-while(left<=mid && right<=last){
-if(arr[left]<arr[right]){
-temp[ptr] = arr[left];
-left++;
-ptr++;
-}
-else{
-temp[ptr] = arr[right];
-right++;
-ptr++;
-}}
-while(left<=mid){
-temp[ptr] = arr[left];
-ptr++;
-left++;}
-while(right<=last){
-temp[ptr] = arr[right];
-ptr++;
-right++;}
-for(i = 0; i<=last; i++){
-arr[i + first] = temp[i];}
-}
 void display_arr(int arr[] , int n){
 int i;
-for(i = 0; i<n; i++){
-printf("%d ",arr[i]);
+for(i =0; i<n; i++){
+printf("%d ",arr[i]);}
+}
+void quick_sort(int arr[] , int first , int last){
+int pindex;
+if(first<last){
+pindex = partition(arr,first,last);
+quick_sort(arr,first, pindex - 1);
+quick_sort(arr,pindex + 1 , last);
 }}
+int partition(int arr[] , int first , int last){
+int pivot,i,j,temp;
+pivot = arr[first];
+i = first;
+j =last;
+while(i<j){
+while(arr[i]<=pivot && i<last){
+i++;}
+while(arr[j]>pivot && j>first){
+j--;}
+if(i<j){
+temp = arr[i];
+arr[i] = arr[j];
+arr[j] = temp;
+} }
+temp = arr[first];
+arr[first] = arr[j];
+arr[j] = temp;
+return j;
+}
+// result : quick sort is implemented and given list of array is sorted 
