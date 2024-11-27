b275b375596b3dcb65767f5111861e469f9023a5
Niraj-Ramnani <neerajramnani800@gmail.com>
2024-11-27 19:56:58 +0530

    all programs were updated

--- AOA lab/aoa8.c
+++ AOA lab/aoa8.c
@@ -1,109 +1,65 @@
-#include <stdio.h>
+//Find Minimum Cost Spanning Tree of a given undirected graph using Kruskal's algorithm.
+#include<stdio.h>
+#include<conio.h>
+#include<stdlib.h>
 
-#define MAX 30
+int i, j, k, a, b, u, v, ne = 1;
+int n, mincost = 0, cost[9][9], parent[9];
+int find(int);
+int uni(int, int);
 
-int Graph[MAX][MAX], i, j, n;
-
-typedef struct edge {
-    int u, v, w;
-} edge;
-
-typedef struct edge_list {
-    edge data[MAX];
-    int n;
-} edge_list;
-
-edge_list elist;
-edge_list spanlist;
-
-void kruskalAlgo();
-int find(int belongs[], int vertexno);
-void applyUnion(int belongs[], int c1, int c2);
-void sort();
-void print();
-
-// Applying Kruskal's Algorithm
-void kruskalAlgo() {
-    int belongs[MAX], cno1, cno2;
-    elist.n = 0;
-
-    for (i = 0; i < n; i++) {
-        for (j = 0; j < n; j++) {
-            if (Graph[i][j] != 0) {
-                elist.data[elist.n].u = i;
-                elist.data[elist.n].v = j;
-                elist.data[elist.n].w = Graph[i][j];
-                elist.n++;
-            }
-        }
-    }
-
-    sort();
-
-    for (i = 0; i < n; i++)
-        belongs[i] = i;
-
-    spanlist.n = 0;
-
-    for (i = 0; i < elist.n; i++) {
-        cno1 = find(belongs, elist.data[i].u);
-        cno2 = find(belongs, elist.data[i].v);
-
-        if (cno1 != cno2) {
-            spanlist.data[spanlist.n] = elist.data[i];
-            spanlist.n++;
-            applyUnion(belongs, cno1, cno2);
-        }
-    }
-}
-
-int find(int belongs[], int vertexno) {
-    return belongs[vertexno];
-}
-
-void applyUnion(int belongs[], int c1, int c2) {
-    for (i = 0; i < n; i++) {
-        if (belongs[i] == c2)
-            belongs[i] = c1;
-    }
-}
-
-// Sorting algorithm
-void sort() {
-    int i, j;
-    edge temp;
-
-    for (i = 0; i < elist.n - 1; i++) {
-        for (j = 0; j < elist.n - 1 - i; j++) {
-            if (elist.data[j].w > elist.data[j + 1].w) {
-                temp = elist.data[j];
-                elist.data[j] = elist.data[j + 1];
-                elist.data[j + 1] = temp;
-            }
-        }
-    }
-}
-
-void print() {
-    printf("Edges in the Minimum Spanning Tree:\n");
-    for (i = 0; i < spanlist.n; i++) {
-        printf("%d -- %d == %d\n", spanlist.data[i].u, spanlist.data[i].v, spanlist.data[i].w);
-    }
-}
-
-int main() {
-    printf("Enter the number of vertices in the graph: ");
+void main(){
+    printf("\n\t Implementation of Kruskal's Algorithm \n");
+    printf("\nEnter number of vertices: ");
     scanf("%d", &n);
 
-    printf("Enter the adjacency matrix of the undirected graph (0 for no edge):\n");
-    for (i = 0; i < n; i++) {
-        for (j = 0; j < n; j++) {
-            scanf("%d", &Graph[i][j]); 
+    printf("Enter the adjacency matrix: \n");
+    for(i = 1; i <= n; i++){
+        for(j = 1; j <= n; j++){
+            scanf("%d", &cost[i][j]);
+            if(cost[i][j] == 0) cost[i][j] = 999;
         }
     }
 
-    kruskalAlgo();
-    print();
+    printf("The edges of the minimum cost spanning tree are: \n");
 
-    return 0; 
+    while(ne < n){
+        int min = 999;
+        for(i = 1; i <= n; i++){
+            for(j = 1; j <= n; j++){
+                if(cost[i][j] < min){
+                    min = cost[i][j];
+                    a = u = i;
+                    b = v = j;
+                }
+            }
+        }
+
+        if(uni(u, v)){
+            printf("Edge %d: (%d, %d) = %d\n", ne++, a, b, min);
+            mincost += min;
+        }
+
+        cost[a][b] = cost[b][a] = 999;
+    }
+
+    printf("\nMinimum cost = %d\n", mincost);
+}
+
+int find(int i){
+    while(parent[i]){
+        i = parent[i];
+    }
+    return i;
+}
+
+int uni(int i, int j){
+    int root_i = find(i);
+    int root_j = find(j);
+
+    if(root_i != root_j){
+        parent[root_j] = root_i;
+        return 1;
+    }
+    return 0;
 }
